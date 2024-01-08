997106805b54ac47972f11c2e029633bcd700e6d
elio dinucci <elio.dinucci92@gmail.com>
2024-01-08 15:40:52 +0100

    data sql cinese

--- src/main/java/it/menu/controller/MenuController.java
+++ src/main/java/it/menu/controller/MenuController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +15,11 @@ import it.menu.entity.LinguaEntity;
 import it.menu.entity.StrutturaEntity;
 import it.menu.service.MenuService;
 
+
+
 @RestController
 public class MenuController {
-	
+	/*@CrossOrigin (origins= "http://localhost:8080") bisognerà mettere 5000 forse il percorso?*/
 	@Autowired
 	MenuService menuService;
 	
